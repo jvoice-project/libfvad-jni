877ff586c9aef1e3aad8daccbf0ef9d53357cabc
Florian Hotze <dev@florianhotze.com>
2026-08-24 15:00:57 +0200

    Add license headers/NOTICE & Apply Spotless formatting (#2)
    - Add license headers to Java source.
    - Add a NOTICE file declaring third-party contents and project license.
    - Add & Apply Spotless with Google Java style.

--- src/main/java/io/github/givimad/libfvadjni/VoiceActivityDetector.java
+++ src/main/java/io/github/givimad/libfvadjni/VoiceActivityDetector.java
@@ -1,232 +1,248 @@
+/*
+ * #%L
+ * libfvad-jni
+ * %%
+ * Copyright (C) 2023 - 2026 Miguel Álvarez Díez & Contributors to libfvad-jni
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package io.github.givimad.libfvadjni;
 
 import io.github.givimad.libfvadjni.internal.NativeUtils;
-
 import java.io.IOException;
 
 /**
- * Libfvad voice activity detector.
+ * libfvad JNI
+ *
+ * <p>libfvad is a voice activity detection engine based on WebRTC's VAD engine.
+ *
+ * @author Miguel Álvarez Díez - Initial contribution
  */
 public class VoiceActivityDetector implements AutoCloseable {
 
-    private static boolean libraryLoaded;
+  private static boolean libraryLoaded;
 
-    private final int pointerRef;
-    private boolean closed;
+  private final int pointerRef;
+  private boolean closed;
 
-    //region native api
-    private static native int fvadNew();
+  // region native api
+  private static native int fvadNew();
 
-    private native void fvadReset(int inst);
+  private native void fvadReset(int inst);
 
-    private native int fvadSetMode(int inst, int mode);
+  private native int fvadSetMode(int inst, int mode);
 
-    private native int fvadSetSampleRate(int inst, int sample_rate);
+  private native int fvadSetSampleRate(int inst, int sample_rate);
 
-    private native int fvadProcess(int inst, short[] frame, int length);
+  private native int fvadProcess(int inst, short[] frame, int length);
 
-    private native void fvadFree(int inst);
+  private native void fvadFree(int inst);
 
-    //endregion
+  // endregion
 
-    private VoiceActivityDetector(int pointerRef) {
-        this.pointerRef = pointerRef;
+  private VoiceActivityDetector(int pointerRef) {
+    this.pointerRef = pointerRef;
+  }
+
+  /**
+   * Sets the vad mode
+   *
+   * @param mode the desired vad mode
+   * @return true when successful
+   * @throws IOException instance is closed
+   */
+  public boolean setMode(Mode mode) throws IOException {
+    assertOpen();
+    return fvadSetMode(pointerRef, mode.ordinal()) == 0;
+  }
+
+  /**
+   * Sets the detector sample rate.
+   *
+   * @param sampleRate desired audio sample rate
+   * @return true when successful
+   * @throws IOException instance is closed
+   */
+  public boolean setSampleRate(SampleRate sampleRate) throws IOException {
+    assertOpen();
+    return fvadSetSampleRate(pointerRef, sampleRate.toValue()) == 0;
+  }
+
+  /**
+   * Process audio frame. Only allow audio frames of 10, 20 or 30 ms. You should calculate the frame
+   * length to use. Formula sampleRate/1000*ms.
+   *
+   * @param frame audio samples
+   * @return true if voice is detected
+   * @throws IOException instance is closed
+   * @throws IllegalArgumentException incorrect frame length
+   */
+  public boolean process(short[] frame) throws IOException, IllegalArgumentException {
+    return process(frame, frame.length);
+  }
+
+  /**
+   * Process audio frame.
+   *
+   * @param frame audio samples
+   * @param length audio samples length
+   * @return true if voice is detected
+   * @throws IOException instance is closed
+   * @throws IllegalArgumentException length parameter is incorrect
+   */
+  public boolean process(short[] frame, int length) throws IOException, IllegalArgumentException {
+    assertOpen();
+    var result = fvadProcess(pointerRef, frame, length);
+    if (result == -1) {
+      throw new IllegalArgumentException("Invalid frame length");
+    }
+    return result == 1;
+  }
+
+  /**
+   * Resets the vad state.
+   *
+   * @throws IOException is detector is closed
+   */
+  public void reset() throws IOException {
+    assertOpen();
+    fvadReset(pointerRef);
+  }
+
+  @Override
+  public void close() {
+    if (!closed) {
+      closed = true;
+      fvadFree(pointerRef);
+    }
+  }
+
+  /** Available sample rates. */
+  public enum SampleRate {
+    /** 8000 samples per seconds */
+    S8000(8000),
+    /** 8000 samples per seconds */
+    S16000(16000),
+    /** 8000 samples per seconds */
+    S32000(32000),
+    /** 8000 samples per seconds */
+    S48000(48000);
+
+    SampleRate(final int value) {
+      this.value = value;
     }
 
+    private final int value;
+
     /**
-     * Sets the vad mode
+     * Get value.
      *
-     * @param mode the desired vad mode
-     * @return true when successful
-     * @throws IOException instance is closed
+     * @return sample rate int value.
      */
-    public boolean setMode(Mode mode) throws IOException {
-        assertOpen();
-        return fvadSetMode(pointerRef, mode.ordinal()) == 0;
+    public int toValue() {
+      return value;
     }
 
     /**
-     * Sets the detector sample rate.
+     * Initialize from number. Only 8000, 16000, 32000, 48000 are allowed.
      *
-     * @param sampleRate desired audio sample rate
-     * @return true when successful
-     * @throws IOException instance is closed
+     * @param value desired sample rate.
+     * @return sample rate enum instance.
+     * @throws IllegalArgumentException unsupported value.
      */
-    public boolean setSampleRate(SampleRate sampleRate) throws IOException {
-        assertOpen();
-        return fvadSetSampleRate(pointerRef, sampleRate.toValue()) == 0;
+    public static SampleRate fromValue(int value) throws IllegalArgumentException {
+      return switch (value) {
+        case 8000 -> S8000;
+        case 16000 -> S16000;
+        case 32000 -> S32000;
+        case 48000 -> S48000;
+        default -> throw new IllegalArgumentException("Unsupported sample rate.");
+      };
     }
+  }
 
-    /**
-     * Process audio frame. Only allow audio frames of 10, 20 or 30 ms.
-     * You should calculate the frame length to use. Formula sampleRate/1000*ms.
-     *
-     * @param frame audio samples
-     * @return true if voice is detected
-     * @throws IOException              instance is closed
-     * @throws IllegalArgumentException incorrect frame length
-     */
-    public boolean process(short[] frame) throws IOException, IllegalArgumentException {
-        return process(frame, frame.length);
+  /** Available vad modes. */
+  public enum Mode {
+    /** Least aggressive about filtering out non-speech */
+    QUALITY,
+    /** Least aggressive about filtering out non-speech */
+    LOW_BITRATE,
+    /** Restrictive in reporting speech */
+    AGGRESSIVE,
+    /** More restrictive in reporting speech */
+    VERY_AGGRESSIVE,
+  }
+
+  /**
+   * Initializes a {@link VoiceActivityDetector} instance.
+   *
+   * @return a new {@link VoiceActivityDetector}.
+   * @throws IOException native library unavailable.
+   */
+  public static VoiceActivityDetector newInstance() throws IOException {
+    assertRegistered();
+    return new VoiceActivityDetector(fvadNew());
+  }
+
+  /**
+   * Register the native library, should be called at first.
+   *
+   * @throws IOException when unable to load the native library
+   */
+  public static void loadLibrary() throws IOException {
+    if (libraryLoaded) {
+      return;
     }
-
-    /**
-     * Process audio frame.
-     *
-     * @param frame  audio samples
-     * @param length audio samples length
-     * @return true if voice is detected
-     * @throws IOException              instance is closed
-     * @throws IllegalArgumentException length parameter is incorrect
-     */
-    public boolean process(short[] frame, int length) throws IOException, IllegalArgumentException {
-        assertOpen();
-        var result = fvadProcess(pointerRef, frame, length);
-        if (result == -1) {
-            throw new IllegalArgumentException("Invalid frame length");
-        }
-        return result == 1;
+    String bundleLibraryPath = null;
+    String osName = System.getProperty("os.name").toLowerCase();
+    String osArch = System.getProperty("os.arch").toLowerCase();
+    if (osName.contains("win")) {
+      if (osArch.contains("amd64") || osArch.contains("x86_64")) {
+        bundleLibraryPath = "/win-amd64/libfvad-jni.dll";
+      }
+    } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
+      if (osArch.contains("amd64") || osArch.contains("x86_64")) {
+        bundleLibraryPath = "/debian-amd64/liblibfvad-jni.so";
+      } else if (osArch.contains("aarch64") || osArch.contains("arm64")) {
+        bundleLibraryPath = "/debian-arm64/liblibfvad-jni.so";
+      } else if (osArch.contains("armv7") || osArch.contains("arm")) {
+        bundleLibraryPath = "/debian-armv7l/liblibfvad-jni.so";
+      }
+    } else if (osName.contains("mac") || osName.contains("darwin")) {
+      if (osArch.contains("amd64") || osArch.contains("x86_64")) {
+        bundleLibraryPath = "/macos-amd64/liblibfvad-jni.dylib";
+      } else if (osArch.contains("aarch64") || osArch.contains("arm64")) {
+        bundleLibraryPath = "/macos-arm64/liblibfvad-jni.dylib";
+      }
     }
-
-    /**
-     * Resets the vad state.
-     *
-     * @throws IOException is detector is closed
-     */
-    public void reset() throws IOException {
-        assertOpen();
-        fvadReset(pointerRef);
+    if (bundleLibraryPath == null) {
+      throw new java.io.IOException(
+          "libfvad-jni: Unsupported platform " + osName + " - " + osArch + ".");
     }
+    NativeUtils.loadLibraryFromJar(bundleLibraryPath);
+    libraryLoaded = true;
+  }
 
-    @Override
-    public void close() {
-        if (!closed) {
-            closed = true;
-            fvadFree(pointerRef);
-        }
+  private static void assertRegistered() throws IOException {
+    if (!libraryLoaded) {
+      throw new IOException("Native library is unavailable.");
     }
+  }
 
-    /**
-     * Available sample rates.
-     */
-    public enum SampleRate {
-        /** 8000 samples per seconds */
-        S8000(8000),
-        /** 8000 samples per seconds */
-        S16000(16000),
-        /** 8000 samples per seconds */
-        S32000(32000),
-        /** 8000 samples per seconds */
-        S48000(48000);
-
-        SampleRate(final int value) {
-            this.value = value;
-        }
-        private final int value;
-
-        /**
-         * Get value.
-         * @return sample rate int value.
-         */
-        public int toValue() {
-            return value;
-        }
-        /**
-         * Initialize from number. Only 8000, 16000, 32000, 48000 are allowed.
-         *
-         * @param value desired sample rate.
-         * @return sample rate enum instance.
-         * @throws IllegalArgumentException unsupported value.
-         */
-        public static SampleRate fromValue(int value) throws IllegalArgumentException {
-            return switch (value) {
-                case 8000 -> S8000;
-                case 16000 -> S16000;
-                case 32000 -> S32000;
-                case 48000 -> S48000;
-                default -> throw new IllegalArgumentException("Unsupported sample rate.");
-            };
-        }
+  private void assertOpen() throws IOException {
+    if (closed) {
+      throw new IOException("VAD is closed.");
     }
-
-    /**
-     * Available vad modes.
-     */
-    public enum Mode {
-        /**
-         * Least aggressive about filtering out non-speech
-         */
-        QUALITY,
-        /**
-         * Least aggressive about filtering out non-speech
-         */
-        LOW_BITRATE,
-        /**
-         * Restrictive in reporting speech
-         */
-        AGGRESSIVE,
-        /**
-         * More restrictive in reporting speech
-         */
-        VERY_AGGRESSIVE,
-
-    }
-
-    /**
-     * Initializes a {@link VoiceActivityDetector} instance.
-     * @return a new {@link VoiceActivityDetector}.
-     * @throws IOException native library unavailable.
-     */
-    public static VoiceActivityDetector newInstance() throws IOException {
-        assertRegistered();
-        return new VoiceActivityDetector(fvadNew());
-    }
-
-    /**
-     * Register the native library, should be called at first.
-     *
-     * @throws IOException when unable to load the native library
-     */
-    public static void loadLibrary() throws IOException {
-        if (libraryLoaded) {
-            return;
-        }
-        String bundleLibraryPath = null;
-        String osName = System.getProperty("os.name").toLowerCase();
-        String osArch = System.getProperty("os.arch").toLowerCase();
-        if (osName.contains("win")) {
-            if (osArch.contains("amd64") || osArch.contains("x86_64")) {
-                bundleLibraryPath = "/win-amd64/libfvad-jni.dll";
-            }
-        } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
-            if (osArch.contains("amd64") || osArch.contains("x86_64")) {
-                bundleLibraryPath = "/debian-amd64/liblibfvad-jni.so";
-            } else if (osArch.contains("aarch64") || osArch.contains("arm64")) {
-                bundleLibraryPath = "/debian-arm64/liblibfvad-jni.so";
-            } else if (osArch.contains("armv7") || osArch.contains("arm")) {
-                bundleLibraryPath = "/debian-armv7l/liblibfvad-jni.so";
-            }
-        } else if (osName.contains("mac") || osName.contains("darwin")) {
-            if (osArch.contains("amd64") || osArch.contains("x86_64")) {
-                bundleLibraryPath = "/macos-amd64/liblibfvad-jni.dylib";
-            } else if (osArch.contains("aarch64") || osArch.contains("arm64")) {
-                bundleLibraryPath = "/macos-arm64/liblibfvad-jni.dylib";
-            }
-        }
-        if (bundleLibraryPath == null) {
-            throw new java.io.IOException("libfvad-jni: Unsupported platform " + osName + " - " + osArch + ".");
-        }
-        NativeUtils.loadLibraryFromJar(bundleLibraryPath);
-        libraryLoaded = true;
-    }
-    private static void assertRegistered() throws IOException {
-        if (!libraryLoaded) {
-            throw new IOException("Native library is unavailable.");
-        }
-    }
-    private void assertOpen() throws IOException {
-        if (closed) {
-            throw new IOException("VAD is closed.");
-        }
-    }
+  }
 }
--- src/test/java/io/github/givimad/libfvadjni/VoiceActivityDetectorTest.java
+++ src/test/java/io/github/givimad/libfvadjni/VoiceActivityDetectorTest.java
@@ -1,100 +1,118 @@
+/*
+ * #%L
+ * libfvad-jni
+ * %%
+ * Copyright (C) 2023 - 2026 Miguel Álvarez Díez & Contributors to libfvad-jni
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package io.github.givimad.libfvadjni;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.file.Path;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Path;
+import java.util.Arrays;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class VoiceActivityDetectorTest {
-    Path samplePath = Path.of("jfk.wav");
-    private VoiceActivityDetector vad;
+  Path samplePath = Path.of("jfk.wav");
+  private VoiceActivityDetector vad;
 
-    @BeforeEach
-    public void before() throws IOException {
-        var sampleFile = samplePath.toFile();
-        if (!sampleFile.exists() || !sampleFile.isFile()) {
-            throw new RuntimeException("Missing sample file");
-        }
-        VoiceActivityDetector.loadLibrary();
-        vad = VoiceActivityDetector.newInstance();
+  @BeforeEach
+  public void before() throws IOException {
+    var sampleFile = samplePath.toFile();
+    if (!sampleFile.exists() || !sampleFile.isFile()) {
+      throw new RuntimeException("Missing sample file");
     }
+    VoiceActivityDetector.loadLibrary();
+    vad = VoiceActivityDetector.newInstance();
+  }
 
-    @AfterEach
-    public void after() throws IOException {
-        vad.close();
+  @AfterEach
+  public void after() throws IOException {
+    vad.close();
+  }
+
+  @Test
+  public void testSetMode() throws IOException {
+    assertTrue(vad.setMode(VoiceActivityDetector.Mode.AGGRESSIVE), "vad mode configured");
+  }
+
+  @Test
+  public void testSetSampleRate() throws IOException {
+    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(8000));
+    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(16000));
+    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(32000));
+    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(48000));
+    assertThrows(
+        IllegalArgumentException.class, () -> VoiceActivityDetector.SampleRate.fromValue(1));
+    assertTrue(
+        vad.setSampleRate(VoiceActivityDetector.SampleRate.S32000), "sample rate configured");
+  }
+
+  @Test
+  public void testReset() throws IOException {
+    vad.reset();
+  }
+
+  @Test
+  public void testVAD() throws IOException, UnsupportedAudioFileException {
+    int sampleRate = 16000;
+    vad.setMode(VoiceActivityDetector.Mode.QUALITY);
+    vad.setSampleRate(VoiceActivityDetector.SampleRate.fromValue(sampleRate));
+    short[] samples = readJFKFileSamples();
+    int samplesLength = samples.length;
+    int step = (sampleRate / 1000) * 10; // 10ms step (only allows 10, 20 or 30ms frame)
+    int detection = 0;
+    for (int i = 0; i < samplesLength - step; i += step) {
+      short[] frame = Arrays.copyOfRange(samples, i, i + step);
+      if (vad.process(frame)) {
+        detection = i;
+        break;
+      }
     }
+    assertEquals(640, detection);
+  }
 
-    @Test
-    public void testSetMode() throws IOException {
-        assertTrue(vad.setMode(VoiceActivityDetector.Mode.AGGRESSIVE), "vad mode configured");
+  private short[] readJFKFileSamples() throws UnsupportedAudioFileException, IOException {
+    // sample is a 16 bit int 16000hz little endian wav file
+    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(samplePath.toFile());
+    // read all the available data to a little endian capture buffer
+    ByteBuffer captureBuffer = ByteBuffer.allocate(audioInputStream.available());
+    captureBuffer.order(ByteOrder.LITTLE_ENDIAN);
+    int read = audioInputStream.read(captureBuffer.array());
+    if (read == -1) {
+      throw new IOException("Empty file");
     }
-
-    @Test
-    public void testSetSampleRate() throws IOException {
-        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(8000));
-        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(16000));
-        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(32000));
-        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(48000));
-        assertThrows(IllegalArgumentException.class, () -> VoiceActivityDetector.SampleRate.fromValue(1));
-        assertTrue(vad.setSampleRate(VoiceActivityDetector.SampleRate.S32000), "sample rate configured");
+    // obtain the 16 int audio samples, short type in java
+    var shortBuffer = captureBuffer.asShortBuffer();
+    short[] samples = new short[captureBuffer.capacity() / 2];
+    var i = 0;
+    while (shortBuffer.hasRemaining()) {
+      samples[i++] = shortBuffer.get();
     }
-
-    @Test
-    public void testReset() throws IOException {
-        vad.reset();
-    }
-
-    @Test
-    public void testVAD() throws IOException, UnsupportedAudioFileException {
-        int sampleRate = 16000;
-        vad.setMode(VoiceActivityDetector.Mode.QUALITY);
-        vad.setSampleRate(VoiceActivityDetector.SampleRate.fromValue(sampleRate));
-        short[] samples = readJFKFileSamples();
-        int samplesLength = samples.length;
-        int step = (sampleRate / 1000) * 10; // 10ms step (only allows 10, 20 or 30ms frame)
-        int detection = 0;
-        for (int i = 0; i < samplesLength - step; i += step) {
-            short[] frame = Arrays.copyOfRange(samples, i, i + step);
-            if (vad.process(frame)) {
-                detection = i;
-                break;
-            }
-        }
-        assertEquals(640, detection);
-    }
-
-    private short[] readJFKFileSamples() throws UnsupportedAudioFileException, IOException {
-        // sample is a 16 bit int 16000hz little endian wav file
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(samplePath.toFile());
-        // read all the available data to a little endian capture buffer
-        ByteBuffer captureBuffer = ByteBuffer.allocate(audioInputStream.available());
-        captureBuffer.order(ByteOrder.LITTLE_ENDIAN);
-        int read = audioInputStream.read(captureBuffer.array());
-        if (read == -1) {
-            throw new IOException("Empty file");
-        }
-        // obtain the 16 int audio samples, short type in java
-        var shortBuffer = captureBuffer.asShortBuffer();
-        short[] samples = new short[captureBuffer.capacity() / 2];
-        var i = 0;
-        while (shortBuffer.hasRemaining()) {
-            samples[i++] = shortBuffer.get();
-        }
-        return samples;
-    }
-
+    return samples;
+  }
 }
