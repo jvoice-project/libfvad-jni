2858a7c42bd66a8b8edc80f3c3beba5cff837dc0
Florian Hotze <dev@florianhotze.com>
2026-08-24 17:10:28 +0200

    Spotless: Sort imports & Sort POM (#4)

--- src/test/java/io/github/givimad/libfvadjni/VoiceActivityDetectorTest.java
+++ src/test/java/io/github/givimad/libfvadjni/VoiceActivityDetectorTest.java
@@ -19,11 +19,6 @@
  */
 package io.github.givimad.libfvadjni;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -32,87 +27,95 @@ import java.util.Arrays;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class VoiceActivityDetectorTest {
-  Path samplePath = Path.of("jfk.wav");
-  private VoiceActivityDetector vad;
+    Path samplePath = Path.of("jfk.wav");
+    private VoiceActivityDetector vad;
 
-  @BeforeEach
-  public void before() throws IOException {
-    var sampleFile = samplePath.toFile();
-    if (!sampleFile.exists() || !sampleFile.isFile()) {
-      throw new RuntimeException("Missing sample file");
+    @BeforeEach
+    public void before() throws IOException {
+        var sampleFile = samplePath.toFile();
+        if (!sampleFile.exists() || !sampleFile.isFile()) {
+            throw new RuntimeException("Missing sample file");
+        }
+        VoiceActivityDetector.loadLibrary();
+        vad = VoiceActivityDetector.newInstance();
     }
-    VoiceActivityDetector.loadLibrary();
-    vad = VoiceActivityDetector.newInstance();
-  }
 
-  @AfterEach
-  public void after() throws IOException {
-    vad.close();
-  }
-
-  @Test
-  public void testSetMode() throws IOException {
-    assertTrue(vad.setMode(VoiceActivityDetector.Mode.AGGRESSIVE), "vad mode configured");
-  }
-
-  @Test
-  public void testSetSampleRate() throws IOException {
-    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(8000));
-    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(16000));
-    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(32000));
-    assertNotNull(VoiceActivityDetector.SampleRate.fromValue(48000));
-    assertThrows(
-        IllegalArgumentException.class, () -> VoiceActivityDetector.SampleRate.fromValue(1));
-    assertTrue(
-        vad.setSampleRate(VoiceActivityDetector.SampleRate.S32000), "sample rate configured");
-  }
-
-  @Test
-  public void testReset() throws IOException {
-    vad.reset();
-  }
-
-  @Test
-  public void testVAD() throws IOException, UnsupportedAudioFileException {
-    int sampleRate = 16000;
-    vad.setMode(VoiceActivityDetector.Mode.QUALITY);
-    vad.setSampleRate(VoiceActivityDetector.SampleRate.fromValue(sampleRate));
-    short[] samples = readJFKFileSamples();
-    int samplesLength = samples.length;
-    int step = (sampleRate / 1000) * 10; // 10ms step (only allows 10, 20 or 30ms frame)
-    int detection = 0;
-    for (int i = 0; i < samplesLength - step; i += step) {
-      short[] frame = Arrays.copyOfRange(samples, i, i + step);
-      if (vad.process(frame)) {
-        detection = i;
-        break;
-      }
+    @AfterEach
+    public void after() throws IOException {
+        vad.close();
     }
-    assertEquals(640, detection);
-  }
 
-  private short[] readJFKFileSamples() throws UnsupportedAudioFileException, IOException {
-    // sample is a 16 bit int 16000hz little endian wav file
-    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(samplePath.toFile());
-    // read all the available data to a little endian capture buffer
-    ByteBuffer captureBuffer = ByteBuffer.allocate(audioInputStream.available());
-    captureBuffer.order(ByteOrder.LITTLE_ENDIAN);
-    int read = audioInputStream.read(captureBuffer.array());
-    if (read == -1) {
-      throw new IOException("Empty file");
+    @Test
+    public void testSetMode() throws IOException {
+        assertTrue(vad.setMode(VoiceActivityDetector.Mode.AGGRESSIVE), "vad mode configured");
     }
-    // obtain the 16 int audio samples, short type in java
-    var shortBuffer = captureBuffer.asShortBuffer();
-    short[] samples = new short[captureBuffer.capacity() / 2];
-    var i = 0;
-    while (shortBuffer.hasRemaining()) {
-      samples[i++] = shortBuffer.get();
+
+    @Test
+    public void testSetSampleRate() throws IOException {
+        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(8000));
+        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(16000));
+        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(32000));
+        assertNotNull(VoiceActivityDetector.SampleRate.fromValue(48000));
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> VoiceActivityDetector.SampleRate.fromValue(1));
+        assertTrue(
+                vad.setSampleRate(VoiceActivityDetector.SampleRate.S32000),
+                "sample rate configured");
     }
-    return samples;
-  }
+
+    @Test
+    public void testReset() throws IOException {
+        vad.reset();
+    }
+
+    @Test
+    public void testVAD() throws IOException, UnsupportedAudioFileException {
+        int sampleRate = 16000;
+        vad.setMode(VoiceActivityDetector.Mode.QUALITY);
+        vad.setSampleRate(VoiceActivityDetector.SampleRate.fromValue(sampleRate));
+        short[] samples = readJFKFileSamples();
+        int samplesLength = samples.length;
+        int step = (sampleRate / 1000) * 10; // 10ms step (only allows 10, 20 or 30ms frame)
+        int detection = 0;
+        for (int i = 0; i < samplesLength - step; i += step) {
+            short[] frame = Arrays.copyOfRange(samples, i, i + step);
+            if (vad.process(frame)) {
+                detection = i;
+                break;
+            }
+        }
+        assertEquals(640, detection);
+    }
+
+    private short[] readJFKFileSamples() throws UnsupportedAudioFileException, IOException {
+        // sample is a 16 bit int 16000hz little endian wav file
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(samplePath.toFile());
+        // read all the available data to a little endian capture buffer
+        ByteBuffer captureBuffer = ByteBuffer.allocate(audioInputStream.available());
+        captureBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        int read = audioInputStream.read(captureBuffer.array());
+        if (read == -1) {
+            throw new IOException("Empty file");
+        }
+        // obtain the 16 int audio samples, short type in java
+        var shortBuffer = captureBuffer.asShortBuffer();
+        short[] samples = new short[captureBuffer.capacity() / 2];
+        var i = 0;
+        while (shortBuffer.hasRemaining()) {
+            samples[i++] = shortBuffer.get();
+        }
+        return samples;
+    }
 }
